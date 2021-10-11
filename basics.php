5b67a7690a7142d1070ede021260c14f314e6cc7
Rani Waad Hermez <1071242@ucn.dk>
2021-10-11 12:42:39 +0200

    commit from rani

--- basics.php
+++ basics.php
@@ -5,7 +5,6 @@ echo 1 . 2, "<br>";                //prints the string "12"
 echo 1.2, "<br>";                  //prints the number 1.2
 echo 1+2, "<br>";
 
-
 $a = 1.234; 
 $b = 1.2e3; 
 $c = 7E-10;
